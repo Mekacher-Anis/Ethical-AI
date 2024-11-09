c0ba9e62235bfd9e3b8210937a61a0de6c4973be
Erik Vogel <erik.vogel@stud.uni-hannover.de>
2024-11-09 14:27:52 +0100

    build inference engine

--- prompting_techniques/dataset_creation.py
+++ prompting_techniques/dataset_creation.py
@@ -1,45 +1,50 @@
+import shutil
+from pathlib import Path
 from typing import Any, Dict, Generator
 
 import datasets
+from constants import ALL_ATTRIBUTES, ATTRIBUTE_DEFINITIONS, BASE_PROMPT_TMPL
 from datasets import load_dataset
-from prompts import ATTRIBUTE_DEFINITIONS, BASE_PROMPT_TMPL
 
 
 def create_zero_shot_dataset(appropriateness_dataset: datasets.Dataset = None):
-    def generate_data_row(row, attribute_name: str, train: bool) -> Dict[str, str]:
-        if train:
-            return {
-                "prompt": BASE_PROMPT_TMPL.format(
-                    attribute_name="attribute_name",
-                    attribute_defintion="attribute_definition",
-                    topic=row["issue"],
-                    argument=row["post_text"],
-                    true_or_false="True" if row[attribute_name] == 1 else "False",
-                ).strip()
-            }
-        else:
-            return {
-                "prompt": BASE_PROMPT_TMPL.format(
-                    attribute_name="attribute_name",
-                    attribute_defintion="attribute_definition",
-                    topic=row["issue"],
-                    argument=row["post_text"],
-                    true_or_false="",
-                ).strip(),
-                "correct_answer": "True" if row[attribute_name] == 1 else "False",
-            }
+    def generate_train_data_row(row, attribute_name: str) -> Dict[str, str]:
+        return {
+            "prompt": BASE_PROMPT_TMPL.format(
+                attribute_name="attribute_name",
+                attribute_definition="attribute_definition",
+                topic=row["issue"],
+                argument=row["post_text"],
+                true_or_false="True" if row[attribute_name] == 1 else "False",
+            ).strip()
+        }
+
+    def generate_eval_data_row(row) -> Dict[str, str]:
+        result = {
+            k: v for k, v in row.items() if k not in ["source_dataset", "fold0.0"]
+        }
+        result["correct_prediction_vector"] = [
+            bool(row[attribute_name]) for attribute_name in ALL_ATTRIBUTES
+        ]
+        return result
 
     # zero-shot dataset creation
     def prompted_dataset_generator(
         train: bool, ds: datasets.Dataset
     ) -> Generator[Dict[str, Any], None, None]:
         def gen():
-            for example in ds["train"]:
-                for (
-                    attribute_name,
-                    attribute_definition,
-                ) in ATTRIBUTE_DEFINITIONS.items():
-                    yield generate_data_row(example, attribute_name, train)
+            if train:
+                for example in ds["train"]:
+                    for (
+                        attribute_name,
+                        attribute_definition,
+                    ) in ATTRIBUTE_DEFINITIONS.items():
+                        yield generate_train_data_row(example, attribute_name)
+            else:
+                for example in ds["validation"]:
+                    yield generate_eval_data_row(
+                        example,
+                    )
 
         return gen
 
@@ -48,12 +53,17 @@ def create_zero_shot_dataset(appropriateness_dataset: datasets.Dataset = None):
         prompted_dataset_generator(train=True, ds=appropriateness_dataset),
         features=datasets.Features({"prompt": datasets.Value("string")}),
     )
+    eval_features = {
+        "issue": datasets.Value("string"),
+        "post_text": datasets.Value("string"),
+        "correct_prediction_vector": datasets.Sequence(datasets.Value("bool")),
+    }
+    for attribute in ALL_ATTRIBUTES:
+        eval_features[attribute] = datasets.Value("bool")
+
     eval_dataset = datasets.Dataset.from_generator(
         prompted_dataset_generator(train=False, ds=appropriateness_dataset),
-        features=datasets.Features({
-            "prompt": datasets.Value("string"),
-            "correct_answer": datasets.ClassLabel(names=["True", "False"]),
-        }),
+        # features=datasets.Features(eval_features),
     )
     # create datasetdict from both datasets
     prompted_dataset = datasets.DatasetDict(
@@ -65,5 +75,7 @@ def create_zero_shot_dataset(appropriateness_dataset: datasets.Dataset = None):
 
 if __name__ == "__main__":
     ds = load_dataset("timonziegenbein/appropriateness-corpus")
+    if Path("zero_shot_dataset").exists():
+        shutil.rmtree("zero_shot_dataset")
 
     create_zero_shot_dataset(appropriateness_dataset=ds)
---
+++ prompting_techniques/inference.py
@@ -0,0 +1,79 @@
+import os
+
+import datasets
+from constants import (ATTRIBUTE_DEFINITIONS, ATTRIBUTE_RELATIONS,
+                       BASE_PROMPT_TMPL)
+from dotenv import load_dotenv
+from tqdm import tqdm
+from transformers import (AutoModelForCausalLM, AutoTokenizer,
+                          BitsAndBytesConfig)
+
+
+def test_model(model, tokenizer, eval_dataset):
+
+    # get token id for "true" and "false" tokens
+    true_token_id = tokenizer.encode("true", return_tensors="pt").to("cuda")[0][1]
+    false_token_id = tokenizer.encode("false", return_tensors="pt").to("cuda")[0][1]
+
+    results = []
+    for i, sample in tqdm(enumerate(eval_dataset)):
+        cur_row = {"post_id": sample["post_id"]}
+
+        for attribute_name, attribute_definition in ATTRIBUTE_DEFINITIONS.items():
+            prompt = BASE_PROMPT_TMPL.format(
+                attribute_name=attribute_name,
+                attribute_definition=attribute_definition,
+                topic=sample["issue"],
+                argument=sample["post_text"],
+                true_or_false="",
+            )
+            tokens = tokenizer.encode(prompt, return_tensors="pt").to("cuda")
+            # get probability distribution vector of next token from LLM
+            model_output = model.generate(
+                tokens,
+                max_new_tokens=1,
+                return_dict_in_generate=True,
+                output_logits=True,
+            )
+            true_logit = model_output["logits"][0][0][true_token_id]
+            false_logit = model_output["logits"][0][0][false_token_id]
+            cur_row[attribute_name] = true_logit > false_logit
+        for attribute_name, attribute_relation in ATTRIBUTE_RELATIONS.items():
+            cur_row[attribute_name] = any(
+                [cur_row[attribute] for attribute in attribute_relation]
+            )
+        results.append(cur_row)
+    return results
+
+
+def load_untrained_llama2_model():
+    load_dotenv()
+
+    quantization_config = BitsAndBytesConfig(
+        load_in_4bit=True, bnb_4bit_quant_type="nf4", bnb_4bit_use_double_quant=True
+    )
+    # pull llama2 model from huggingface
+    tokenizer = AutoTokenizer.from_pretrained(
+        "meta-llama/Llama-2-7b-hf",
+        token=os.getenv("HF_TOKEN"),
+        device_map="auto",
+        config=quantization_config,
+    )
+    model = AutoModelForCausalLM.from_pretrained(
+        "meta-llama/Llama-2-7b-hf",
+        token=os.getenv("HF_TOKEN"),
+        device_map="auto",
+        config=quantization_config,
+        torch_dtype="float16",
+    )
+    return tokenizer, model
+
+
+if __name__ == "__main__":
+    tokenizer, model = load_untrained_llama2_model()
+    print("Model loaded")
+    # load dataset from file
+    zero_shot_dataset = datasets.load_from_disk("zero_shot_dataset")
+    eval_dataset = zero_shot_dataset["validation"]
+    results = test_model(model, tokenizer, eval_dataset)
+    print(results)
