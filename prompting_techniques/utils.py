432afd37367ea2db5821bfc36956991d9a1533c6
Erik Vogel <erik.vogel@stud.uni-hannover.de>
2024-11-26 11:22:37 +0100

    formatting

--- prompting_techniques/utils.py
+++ prompting_techniques/utils.py
@@ -17,9 +17,9 @@ def load_untrained_llama2_model(
         load_only_tokenizer and load_only_model
     ), "Cannot load only tokenizer and only model at the same time"
     load_both = not load_only_tokenizer and not load_only_model
-    #quantization_config = custom_quantization_config or BitsAndBytesConfig(
+    # quantization_config = custom_quantization_config or BitsAndBytesConfig(
     #    load_in_4bit=True, bnb_4bit_quant_type="nf4", bnb_4bit_use_double_quant=True
-    #)
+    # )
     if load_only_tokenizer or load_both:
         tokenizer = AutoTokenizer.from_pretrained(
             "meta-llama/Llama-2-7b-hf",
@@ -32,7 +32,7 @@ def load_untrained_llama2_model(
             "meta-llama/Llama-2-7b-hf",
             token=os.getenv("HF_TOKEN"),
             device_map="cuda",
-            #config=quantization_config,
+            # config=quantization_config,
             torch_dtype="float16",
         )
     if load_only_tokenizer:
