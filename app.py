506d66c86419bb7f4f089fd177c7e950ae36445b
JulianPMenon <97676461+JulianPMenon@users.noreply.github.com>
2024-12-11 18:00:02 +0100

    Frontend without backand end call funktion for model

--- app.py
+++ app.py
@@ -1,22 +1,41 @@
+#installs
+
 #imports
 import streamlit as st
 import pandas as pd
 import numpy as np
 
+
+def predict_on_input(input):
+    #call model here
+    result = input
+
+    inappropriet = False
+    return result, inappropriet
+
 def main():
     st.title("Ethical-Artificial-Inteligence")
     menu = ["Home","About"]
-    choice = st.sidebar.secectbox("Menu",menu)
+    choice = st.sidebar.selectbox("Menu",menu)
     
     if choice == "Home":
-        st.subheader("Approprietness")
+        st.subheader("Check an argument for Inapproprietness")
         
         with st.form(key='myform'):
             raw_text = st.text_area("Input an argument here.")
-            submit_text = st.form_submit_button
-        
+            submit_text = st.form_submit_button(label='Submit')
+
+        if submit_text:
+            #process the text
+            output, result = predict_on_input(raw_text)
+            if result:
+                st.error("Inappropriet")
+            else:
+                st.success("Appropriet")
+            st.text_area(label='Output',value=output,disabled=True)
+
     else:
         st.subheader("About")
         
-    if __name__ == '__main__':
-        main()
+if __name__ == '__main__':
+    main()
