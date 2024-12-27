3df76ba5d492046e4fc287fc1d53c1e53fd08c75
JulianPMenon <97676461+JulianPMenon@users.noreply.github.com>
2024-12-27 17:53:42 +0100

    Addet a way to show the different influenzes by threshold per dimension

--- app.py
+++ app.py
@@ -6,12 +6,87 @@ import pandas as pd
 import numpy as np
 from annotated_text import annotated_text
 
+CATEGORIES = [
+    #Threshold tags for influential tokens 
+    "","Weak Influenze","Medium Influenze","Strong Influenze"
+]
+THRESHOLDS = [
+    #needs to be edited to match actual thresholds
+    0, 0.5 , 1
+]
+
+TESTINPUT = {
+  "Inappropiateness":
+  [
+    {"token": "Du", "einfluss_wrt": 0.2},
+    {"token": "Hurensohn", "einfluss_wrt": 0.7}
+  ],
+  "Toxic Emotions": 
+  [
+    {"token": "Du","einfluss_wrt": 0},
+    {"token": "Hurensohn", "einfluss_wrt": 1}
+  ]
+}
+
+''' 
+    Here the model can be called to generate a responce on the input.
+    The model predictes if a text is Innaprpriate or not and uses Captum to determin which tokens are influencing the decision.
+    The Influence is formatet in a dictionary: {"Dimension of Innapropriateness": [{"token": "actul_token", "einfluss_wrt": float}]}
+
+    @input      the raw text input 
+    @return     the input that was given, a Bool True if input was innapropreate, the formated tuples using build_tuples for influential tokens marked for anoteted_text
+'''
 def predict_on_input(input):
     #call model here
-    result = input
+    
 
-    inappropriet = False
-    return result, inappropriet
+    inappropriate = True
+    
+    #captum answer as a dictionary formated like TESTINPUT
+
+    captum_out = TESTINPUT
+
+
+    return input, inappropriate, build_tuples(captum_out)
+
+
+'''
+    Building a formatet string with annotated tokens using annotated_text from streamlit. 
+    The annotations show how high the influenz on the decition of the models was per token based on thresholds.
+
+    @tup input list of tuples with ("token", float)
+
+
+'''
+def build_string(tup):
+    copy = tup
+    updated_list = []
+    for token, value in copy:
+        if value <= 0:
+            updated_list.append(token)
+        else:
+            replacement = next((rep for t, rep in zip(THRESHOLDS, CATEGORIES) if value <= t), CATEGORIES[-1])
+        
+            updated_list.append((token, replacement))
+        
+    
+    #put the formated string out
+    annotated_text(updated_list)    
+
+    return
+
+'''
+    Transfoming the captum dictionary in a form usuable by annotated_text a dictionary of lists of tuples.
+    The entries of the dictionary are sorted by categorie. Each list is to be used with annotated text seperately.
+
+    @dic input dictionary formated like TESTINPUT
+'''
+def build_tuples(dic):
+    #build a dictionary of touples
+    tuples = {}
+    for category, entries in dic.items():
+        tuples[category] = [(entry['token'], entry['einfluss_wrt']) for entry in entries]
+    return tuples  
 
 def main():
     st.title("Ethical-Artificial-Inteligence")
@@ -27,13 +102,20 @@ def main():
 
         if submit_text:
             #process the text
-            output, result = predict_on_input(raw_text)
+            output, result, category_tuples = predict_on_input(raw_text)
             if result:
                 st.error("Inappropriate")
             else:
                 st.success("Appropriate")
-            #st.text_area(label='Output',value=output,disabled=True)
-            annotated_text("This", ("fugly", "Toxic Emotions"), "looking", ( "idiot","Toxic Emotions"), ("!!!!!", "Exessiv Intensity"))
+            
+            #test method taking the testinput and iterating through the dictionary
+            
+            for category,entry in category_tuples.items():
+                st.text(f"{category}: ")
+                build_string(entry)
+                
+
+
 
     else:
         st.subheader("About")
